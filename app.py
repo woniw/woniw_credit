d08981239584f3adf3bb24c64029f2d68f22cc7f
woniw <woniwwinow@gmail.com>
2025-07-21 23:42:42 -0600

    working login form completed login form logic

--- app.py
+++ app.py
@@ -24,15 +24,22 @@ def index():
         print(f"{red}password: {password}")
 
         if username not in data['users']:
+            print(f"{red}-----------------------------")
             print(f"{red} user does not exist")
+            print(f"{red}----------------------------")
             invalid_username_message = "Invalid username"
             return render_template('index.html', invalid_username_message=invalid_username_message)
         elif username in data['users']:
             if password == data['users'][username]["important"]['password']:
-                print("-----------------------------")
+                print(f"{green}-----------------------------")
                 print(f"{green} password is correct!")
-                print("----------------------------")
-
+                print(f"{green}----------------------------")
+            elif password != data['users'][username]["important"]['password']:
+                print(f"{red}-----------------------------")
+                print(f"{red} password is incorrect!")
+                print(f"{red}----------------------------")
+                invalid_password_message = "Invalid Password"
+                return render_template("index.html", invalid_password_message=invalid_password_message)
     return render_template('index.html')
 
 if __name__ == "__main__":
