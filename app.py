ad324fcea7baeaad799a58ccde26016f116c61e1
woniw <woniwwinow@gmail.com>
2025-07-21 23:37:44 -0600

    first login logic test prototype

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+# library imports
 from flask import Flask, render_template, request, jsonify, url_for, redirect
-from data.variables import data
 import logging
+
+from data.variables import data
+
+#! COLOR IMPORTS
+from data.variables import red
+from data.variables import green
 
 print(f"LOG: {data}")
 app = Flask(__name__)
@@ -13,13 +19,21 @@ def index():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        
-        from data.variables import red
+
         print(f"{red}username: {username}")
         print(f"{red}password: {password}")
 
-    return render_template('index.html')
+        if username not in data['users']:
+            print(f"{red} user does not exist")
+            invalid_username_message = "Invalid username"
+            return render_template('index.html', invalid_username_message=invalid_username_message)
+        elif username in data['users']:
+            if password == data['users'][username]["important"]['password']:
+                print("-----------------------------")
+                print(f"{green} password is correct!")
+                print("----------------------------")
 
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
