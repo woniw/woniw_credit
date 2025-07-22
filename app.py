9f72eee405ca2d1fc78ab6b2db7613e135d55ae4
woniw <woniwwinow@gmail.com>
2025-07-21 22:48:29 -0600

    input box data can now be captured was done in app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 from data.variables import data
+import logging
 
 print(f"LOG: {data}")
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    username = None
+    password = None
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        from data.variables import red
+        print(f"{red}username: {username}")
+        print(f"{red}password: {password}")
+
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- data/variables.py
+++ data/variables.py
@@ -1,3 +1,23 @@
 from data.json_management import load_json
 
 data = load_json("data.json")
+
+# Text colors
+black  = "\033[30m"
+red    = "\033[31m"
+green  = "\033[32m"
+yellow = "\033[33m"
+blue   = "\033[34m"
+magenta = "\033[35m"
+cyan   = "\033[36m"
+white  = "\033[37m"
+
+# Bright colors
+bright_black  = "\033[90m"
+bright_red    = "\033[91m"
+bright_green  = "\033[92m"
+bright_yellow = "\033[93m"
+bright_blue   = "\033[94m"
+bright_magenta = "\033[95m"
+bright_cyan   = "\033[96m"
+bright_white  = "\033[97m"
